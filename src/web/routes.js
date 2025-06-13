072b698055ce615b2064c2d9aa68c78d6b014d44
zulfikar2022 <sayedzulfikar2019@gmail.com>
2025-06-13 11:37:58 +0600

    fix: update logout route to use correct path and adjust links in views

--- src/web/routes.js
+++ src/web/routes.js
@@ -2,12 +2,16 @@ import express from "express";
 import { webControllers } from "./controllers.js";
 import { loggedInUsersOnly } from "../middlewares/loggedInUsersOnly.js";
 import { loggedOutUsersOnly } from "../middlewares/loggedOutUsersOnly.js";
+import { userControllers } from "../modules/user/user.controller.js";
 
 const router = express.Router();
 
 router.get("/", webControllers.homePage);
+
 router.get("/profile", loggedInUsersOnly, webControllers.profilePage);
 router.get("/dashboard", loggedInUsersOnly, webControllers.dashboardPage);
+router.post("/logout", loggedInUsersOnly, userControllers.logoutUser);
+
 router.get("/login", loggedOutUsersOnly, webControllers.loginPage);
 router.get("/register", loggedOutUsersOnly, webControllers.registerPage);
 
